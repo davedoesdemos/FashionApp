b7a236421e38dbffc7378c675772d7f2c4b25863
Dave Lusty <35692266+dalusty@users.noreply.github.com>
2024-03-18 12:07:43 +0000

    fix

--- Website2/js/index.js
+++ Website2/js/index.js
@@ -4,8 +4,8 @@ window.addEventListener('DOMContentLoaded', pageFirstLoad, false);
 async function pageFirstLoad() {
     userInfo = await getUserInfo();
     if (userInfo.clientPrincipal != null) {
-        document.getElementById("rightColumn").innerHTML = userInfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
+        document.getElementById("loginbox").innerHTML = userInfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
     } else {
-        document.getElementById("rightColumn").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
+        document.getElementById("loginbox").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
     }
 }
--- Website2/js/swipe.js
+++ Website2/js/swipe.js
@@ -13,9 +13,9 @@ async function pageFirstLoad() {
     document.addEventListener("touchstart", onMouseDown);
     userInfo = await getUserInfo();
     if (userInfo.clientPrincipal != null) {
-        document.getElementById("rightColumn").innerHTML = userInfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
+        document.getElementById("loginbox").innerHTML = userInfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
     } else {
-        document.getElementById("rightColumn").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
+        document.getElementById("loginbox").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
     }
 }
 
