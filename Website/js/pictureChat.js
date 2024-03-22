129a932d5a24efb3063e800e940f13fbbf276b2d
Dave Does Demos <46533954+davedoesdemos@users.noreply.github.com>
2024-03-22 16:20:23 +0000

    added user data to picchat

--- Website/js/pictureChat.js
+++ Website/js/pictureChat.js
@@ -4,6 +4,12 @@ const chatHistory = [];
 window.addEventListener('DOMContentLoaded', pageFirstLoad, false);
 
 async function pageFirstLoad() {
+    userInfo = await getUserInfo();
+    if (userInfo.clientPrincipal != null) {
+        document.getElementById("loginbox").innerHTML = userInfo.clientPrincipal.userDetails + " <a href=\"/.auth/logout\">(Logout)</a>";
+    } else {
+        document.getElementById("loginbox").innerHTML = "<a href=\"/.auth/login/aadb2c\">Login</a>";
+    }
     document.querySelector("#inputFile").addEventListener("change", readFile);
 }
 
